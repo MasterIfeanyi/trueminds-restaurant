07ef93fb24855b3e2dae8196ee6f29b58f1e679d
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-02-12 23:44:55 +0100

    fix Layout

--- src/app/router/AppRouter.jsx
+++ src/app/router/AppRouter.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '@/pages/Home/Home'
 import Dashboard from '@/pages/Dashboard/Dashboard'
-import MainLayout from '@/components/Layout/MainLayout'
+import Layout from '../../Layout/Layout'
+
+// import MainLayout from '@/components/Layout/MainLayout'
 
 export function AppRouter() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
+    <Layout>
+        <Routes>
 
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+            {/* <Route element={<MainLayout />}>
+            </Route> */}
+
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    </Layout>
   )
 }
